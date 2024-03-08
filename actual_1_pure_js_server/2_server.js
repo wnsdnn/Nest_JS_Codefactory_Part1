ff70a3a2fe8dd03d95a68113ba065810c4926120
wnsdnn <a24369029@gmail.com>
2024-03-08 23:31:15 +0900

    Express로 REST API 구현해보기

---
+++ actual_1_pure_js_server/2_server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1>');
+});
+
+app.get('/post', (req, res) => {
+    res.send('<h1>Post Page</h1>');
+});
+
+app.use((req, res) => {
+    res.status(404).send('<h1>404 Page Not Found!</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('server running on http://localhost:3000');
+});
