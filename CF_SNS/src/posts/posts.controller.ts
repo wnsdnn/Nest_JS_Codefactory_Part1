abccae655bbbcbebd673feffe7f692849caae8af
wnsdnn <a24369029@gmail.com>
2024-03-09 23:18:22 +0900

    Get Posts 구현하기

--- CF_SNS/src/posts/posts.controller.ts
+++ CF_SNS/src/posts/posts.controller.ts
@@ -8,7 +8,8 @@ import { PostsService } from './posts.service';
  * likeCount: number;
  * commentCount: number;
  */
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -16,18 +17,54 @@ interface Post {
   commentCount: number;
 }
 
+const posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 1000000,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '노래 연습 하고 있는 해린',
+    likeCount: 1000000,
+    commentCount: 999999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '종합운동자에서 공연중인 로제',
+    likeCount: 1000000,
+    commentCount: 999999,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // 1) GET / posts
+  // 모든 post를 다 가져온다.
   @Get('')
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 100000000,
-      commentCount: 999999,
-    };
+  getPosts() {
+    return posts;
   }
+
+  // 2) GET /posts/:id
+  // id에 해당되는 post를 가져온다.
+  // 예를 들어 id: 1인경구 id가 1인 포스트를 가져온다.
+
+  // 3) POST /posts
+  // POST를 생성한다.
+
+  // 4) PUT /posts/:id
+  // id에 해당되는 POST를 변경한다.
+
+  // 5) DELETE /posts/:id
+  // id에 해당되는 POST를 삭제한다.
 }
