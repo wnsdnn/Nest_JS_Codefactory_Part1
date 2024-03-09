7f1e652786570db715695392ab36401dcbdcb8ea
wnsdnn <a24369029@gmail.com>
2024-03-09 22:54:43 +0900

    Posts 모듈 생성하기

--- CF_SNS/src/app.controller.ts
+++ CF_SNS/src/app.controller.ts
@@ -1,33 +1,9 @@
-import {Controller, Get, Post} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-
-/**
- * author: string;
- * title: string;
- * content: string;
- * likeCount: number;
- * commentCount: number;
- */
-interface Post {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('post')
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 100000000,
-      commentCount: 999999,
-    };
-  }
 }
+
+// nest g resource => 자동으로 모듈 파일 생성
