dd9a2d21fdd5bd7a749a039782bcb64ef1c29da2
wnsdnn <a24369029@gmail.com>
2024-03-09 16:32:45 +0900

    데코레이터에 Path 추가하기

--- CF_SNS/src/app.controller.ts
+++ CF_SNS/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 
 /**
@@ -20,7 +20,7 @@ interface Post {
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('post')
   getPost(): Post {
     return {
       author: 'newjeans_official',
