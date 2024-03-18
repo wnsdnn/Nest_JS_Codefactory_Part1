b8e0ce71dd6463912c1dcb9774f7042ea8f2ff3d
wnsdnn <a24369029@gmail.com>
2024-03-18 21:36:11 +0900

    Relation Options

--- inflearn_actual/src/app.controller.ts
+++ inflearn_actual/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role, UserModel } from './entity/user.entity';
 import { Repository } from 'typeorm';
@@ -29,12 +29,7 @@ export class AppController {
 
   @Get('users')
   getUsers() {
-    return this.userRepository.find({
-      relations: {
-        profile: true,
-        posts: true,
-      },
-    });
+    return this.userRepository.find();
   }
 
   @Patch('users/:id')
@@ -51,16 +46,24 @@ export class AppController {
     });
   }
 
+  @Delete('user/profile/:id')
+  async deleteProfile(@Param('id') id: string) {
+    await this.profileRepository.delete(+id);
+  }
+
   @Post('user/profile')
   async createUserAndProfile() {
     const user = await this.userRepository.save({
       email: 'asdf@codefactory.ai',
+      profile: {
+        profileImg: 'asdf.jpg',
+      },
     });
 
-    const profile = await this.profileRepository.save({
-      profileImg: 'asdf.jpg',
-      user: user,
-    });
+    // const profile = await this.profileRepository.save({
+    //   profileImg: 'asdf.jpg',
+    //   user: user,
+    // });
 
     return user;
   }
--- inflearn_actual/src/entity/user.entity.ts
+++ inflearn_actual/src/entity/user.entity.ts
@@ -98,7 +98,21 @@ export class UserModel {
   additionalId: string;
 
   // ProfileModel과 1대1 연동
-  @OneToOne(() => ProfileModel, (profile) => profile.user)
+  @OneToOne(() => ProfileModel, (profile) => profile.user, {
+    // find() 실행 할때마다 항상 같이 가져올 relation
+    eager: true,
+    // 저장할때 relation을 한번에 같이 저장가능
+    cascade: true,
+    // null이 가능한지
+    nullable: true,
+    // 관계가 삭제했을때
+    // no action -> 아무것도 안함
+    // cascade -> 참조하는 Row도 같이 삭제
+    // set null -> 참조하는 Row에서 참조 id를 null로 변경
+    // default -> 기본 세팅으로 설정 (테이블의 기본 세팅)
+    // restrict -> 참조하고 있는 Row가 있는경우 참조당하는 Row 삭제 불가
+    onDelete: 'RESTRICT',
+  })
   @JoinColumn()
   profile: ProfileModel;
 
