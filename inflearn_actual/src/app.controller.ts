5eacc0c3ff6cb25a7cc0f362775216a4d5f6987c
wnsdnn <a24369029@gmail.com>
2024-03-18 23:29:24 +0900

    Typeorm 유틸리티 탐구

--- inflearn_actual/src/app.controller.ts
+++ inflearn_actual/src/app.controller.ts
@@ -1,7 +1,18 @@
 import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role, UserModel } from './entity/user.entity';
-import { Repository } from 'typeorm';
+import {
+  Between,
+  Equal,
+  ILike, In, IsNull,
+  LessThan,
+  LessThanOrEqual,
+  Like,
+  MoreThan,
+  MoreThanOrEqual,
+  Not,
+  Repository
+} from "typeorm";
 import { ProfileModel } from './entity/profile.entity';
 import { PostModel } from './entity/post.entity';
 import { TagModel } from './entity/tag.entity';
@@ -20,35 +31,60 @@ export class AppController {
   ) {}
 
   @Post('users')
-  postUser() {
-    return this.userRepository.save({
-      email: '1234@gmail.com',
-      // title: 'test title',
-      // role: 'another role',
-    });
+  async postUser() {
+    for (let i = 0; i < 100; i++) {
+      await this.userRepository.save({
+        email: `user-${i}@google.com`,
+      });
+    }
+    // return this.userRepository.save({
+    //   email: '1234@gmail.com',
+    //   // title: 'test title',
+    //   // role: 'another role',
+    // });
   }
 
   @Get('users')
   getUsers() {
     return this.userRepository.find({
+      // 필터링할 조건을 입력하게된다. (AND 조건)
+      where: {
+        // Not() - 아닌경우 가져오기
+        // id: Not(1),
+        // LessThan() - 적은경우 가져오기
+        // id: LessThan(30),
+        // LessThanOrEqual() - 적은경우 or 같은경우
+        // id: LessThanOrEqual(30),
+        // MoreThan() - 많은경우
+        // id: MoreThan(30),
+        // MoreThanOrEqual() - 많거나 같은경우
+        // id: MoreThanOrEqual(30),
+        // Equal() - 같은경우
+        // id: Equal(30),
+        // Like() - 유사값
+        // email: Like('%0%'),
+        // ILike() - 대문자 소문자 구분안하는 유사값
+        // email: ILike('%GOOGLE%'),
+        //Between() - 사이값
+        // id: Between(10, 15),
+        // In() - 해당되는 여러개의 값
+        // id: In([1, 3, 5, 7, 99]),
+        // IsNull() - null인 경우
+        // id: IsNull(),
+      },
       // 어떤 프로퍼티를 선택할지
       // 기본은 모든 프로퍼티를 가져온다
       // 만약에 select를 정의하지 않으면
       //
       // select를 정의하면 정의된 프로퍼티들만 가져오게된다.
-      select: {
-        id: true,
-        createdAt: true,
-        updatedAt: true,
-        version: true,
-        profile: {
-          id: true,
-        },
-      },
-      // 필터링할 조건을 입력하게된다. (AND 조건)
-      // where: {
+      // select: {
+      //   id: true,
+      //   createdAt: true,
+      //   updatedAt: true,
+      //   version: true,
       //   profile: {
-      //     id: 3,
+      //     id: true,
+      //     id: true,
       //   },
       // },
       // 리스트로 값을 넣을시 OR조건으로 변경된다.
@@ -63,19 +99,19 @@ export class AppController {
       // 관계를 가져오는법
       // relations에 선언한 프로퍼티는
       // select나 where에서 사용할수 있음
-      relations: {
-        profile: true,
-      },
+      // relations: {
+      //   profile: true,
+      // },
       // 오름차순, 내림차순
       // ASC -> 오름차순
       // DESC -> 내림차순
-      order: {
-        id: 'DESC',
-      },
+      // order: {
+      //   id: 'DESC',
+      // },
       // 처음 몇개를 제외할지 (default: 0),
-      skip: 0,
+      // skip: 0,
       // 몇개를 가져올지 (default: 0[테이블 전체 행의 개수])
-      take: 1,
+      // take: 1,
     });
   }
 
