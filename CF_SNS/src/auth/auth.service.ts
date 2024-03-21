e3197d7cd3e151302c3bc82248cff622a5ef8c2d
wnsdnn <a24369029@gmail.com>
2024-03-21 22:07:57 +0900

    토큰 signing 해보기

--- CF_SNS/src/auth/auth.service.ts
+++ CF_SNS/src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModel } from '../users/entities/users.entity';
+import { JWT_SECRET } from './const/auth.const';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
   /**
    * 우리가 만드려는 기능
    *
@@ -27,4 +32,26 @@ export class AuthService {
    *      3. 모두 통과되면 찾은 사용자 정보 반환
    *      4. loginWithEmail에서 반환된 데이터를 기반으로 토큰 생성
    */
+
+  /**
+   * Payload에 들어갈 정보
+   *
+   * 1) email
+   * 2) sub -> id
+   * 3) type: 'access' | 'refresh'
+   */
+  // Pick -> UsersModel에서 email과 id값을 가져온다
+  signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: JWT_SECRET,
+      // seconds
+      expiresIn: isRefreshToken ? 3600 : 300,
+    });
+  }
 }
