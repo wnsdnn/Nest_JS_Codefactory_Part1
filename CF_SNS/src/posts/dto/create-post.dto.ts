1b78b6944dba837654d8e98bfd55706388127bb2
wnsdnn <a24369029@gmail.com>
2024-03-27 23:24:30 +0900

    Class Validator 에러 메시지 변경하기

--- CF_SNS/src/posts/dto/create-post.dto.ts
+++ CF_SNS/src/posts/dto/create-post.dto.ts
@@ -1,9 +1,13 @@
 import { IsString } from 'class-validator';
 
 export class CreatePostDto {
-  @IsString()
+  @IsString({
+    message: 'title은 string 타입을 입력해줘야 합니다.',
+  })
   title: string;
 
-  @IsString()
+  @IsString({
+    message: 'content는 string 타입을 입력해줘야 합니다.',
+  })
   content: string;
 }
