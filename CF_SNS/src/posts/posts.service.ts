8a47598cf9c92ea335f0ae35643b37df331d9e86
wnsdnn <a24369029@gmail.com>
2024-04-01 22:55:35 +0900

    Override Options 사용해보기

--- CF_SNS/src/posts/posts.service.ts
+++ CF_SNS/src/posts/posts.service.ts
@@ -51,7 +51,14 @@ export class PostsService {
 
   // 오름차순으로 정렬하는 pagination만 구현한다.
   async paginatePosts(dto: PaginatePostDto) {
-    return this.commonService.paginate(dto, this.postsRepository, {}, 'posts');
+    return this.commonService.paginate(
+      dto,
+      this.postsRepository,
+      {
+        relations: ['author'],
+      },
+      'posts',
+    );
     // if (dto.page) {
     //   return this.pagePaginatePosts(dto);
     // } else {
