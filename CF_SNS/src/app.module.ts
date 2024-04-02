b75efa4b91f62f2a50650e78ff1e0e3c6879473d
wnsdnn <a24369029@gmail.com>
2024-04-02 21:07:26 +0900

    process 객체를 이용해서 환경변수 불러오기

--- CF_SNS/src/app.module.ts
+++ CF_SNS/src/app.module.ts
@@ -10,6 +10,14 @@ import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
+import {
+  ENV_DB_DATABASE_KEY,
+  ENV_DB_HOST_KEY,
+  ENV_DB_PASSWORD_KEY,
+  ENV_DB_PORT_KEY,
+  ENV_DB_USERNAME_KEY,
+} from './common/const/env-keys.const';
 
 @Module({
   // imports - 다른 모듈을 불러올때 사용
@@ -26,11 +34,12 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       // 데이터베이스 타입
       type: 'postgres',
-      host: '127.0.0.1',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
+      // process - 환경변수값을 가져올수 있음
+      host: process.env[ENV_DB_HOST_KEY],
+      port: parseInt(process.env[ENV_DB_PORT_KEY]),
+      username: process.env[ENV_DB_USERNAME_KEY],
+      password: process.env[ENV_DB_PASSWORD_KEY],
+      database: process.env[ENV_DB_DATABASE_KEY],
       // entity를 등록
       entities: [PostsModel, UsersModel],
       synchronize: true,
