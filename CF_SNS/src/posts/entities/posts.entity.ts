fe0efbeb1f84a5d9d0cd0ec2b8eb30b53a669cd9
wnsdnn <a24369029@gmail.com>
2024-04-02 22:55:41 +0900

    Class Transformer 이용해서 URL에 prefix 추가해주기

--- CF_SNS/src/posts/entities/posts.entity.ts
+++ CF_SNS/src/posts/entities/posts.entity.ts
@@ -3,6 +3,9 @@ import { UsersModel } from '../../users/entities/users.entity';
 import { BaseModel } from '../../common/entity/base.entity';
 import { IsString } from 'class-validator';
 import { stringValidationMessage } from '../../common/validation-message/string-validation.message';
+import { Transform } from 'class-transformer';
+import * as path from 'path';
+import { POST_PUBLIC_IMAGE_PATH } from '../../common/const/path.const';
 
 @Entity()
 export class PostsModel extends BaseModel {
@@ -33,6 +36,10 @@ export class PostsModel extends BaseModel {
   @Column({
     nullable: true,
   })
+  // 자동으로 값 변경
+  @Transform(
+    ({ value }) => value && `/${path.join(POST_PUBLIC_IMAGE_PATH, value)}`,
+  )
   image?: string;
 
   @Column()
