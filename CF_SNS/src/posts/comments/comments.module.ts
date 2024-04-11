4bbe925523b6ba20b402034819c5bc5ec619e0cd
wnsdnn <a24369029@gmail.com>
2024-04-11 22:34:23 +0900

    PostExists Middleware CommentsController에 적용하기

--- CF_SNS/src/posts/comments/comments.module.ts
+++ CF_SNS/src/posts/comments/comments.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,6 +6,8 @@ import { CommentsModel } from './entity/comments.entity';
 import { CommonModule } from '../../common/common.module';
 import { AuthModule } from '../../auth/auth.module';
 import { UsersModule } from '../../users/users.module';
+import { PostExistsMiddleware } from './middleware/post-exists.middleware';
+import { PostsModule } from '../posts.module';
 
 @Module({
   imports: [
@@ -14,9 +16,17 @@ import { UsersModule } from '../../users/users.module';
     CommonModule,
     AuthModule,
     UsersModule,
+    PostsModule,
   ],
   exports: [CommentsService],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
-export class CommentsModule {}
+export class CommentsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(PostExistsMiddleware)
+      // 적용하고 싶은 Controller를 통으로 넣어줘도 됨 
+      .forRoutes(CommentsController);
+  }
+}
